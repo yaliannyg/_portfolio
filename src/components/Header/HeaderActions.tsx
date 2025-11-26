1c6ecca4733c5ae2bc3138735451f6df084af1ac
yaligonz <yalianny@atomicpixels.io>
2025-11-26 15:58:13 -0300

    cv update

--- src/components/Header/HeaderActions.tsx
+++ src/components/Header/HeaderActions.tsx
@@ -1,10 +1,13 @@
-import CVDocument from "@/assets/documents/YALIANNY  GONZALEZ.pdf";
 import { LetsTalkBtn } from "@/App";
 
 function HeaderActions() {
   return (
     <div className="flex gap-5 mt-10">
-      <a className="btn" href={CVDocument} download>
+      <a
+        className="btn"
+        href="https://drive.google.com/file/d/15hWtwq-Hl11xu8ASCVWs4yCK4t5nd8x4/view?usp=sharing"
+        target="_blank"
+      >
         Download CV
       </a>
       <LetsTalkBtn />
