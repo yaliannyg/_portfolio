6f9002e41bc35a25e4297b4d81f47ed8b2b634ab
yaligonz <yalianny@atomicpixels.io>
2025-11-26 15:58:38 -0300

    grid dimensions update

--- src/components/Work/WorkSection.tsx
+++ src/components/Work/WorkSection.tsx
@@ -26,9 +26,9 @@ function SectionWork() {
   return (
     <div className="mt-10">
       <SectionsTitle title="portfolio" subtitle="my work" />
-      <div className="flex flex-wrap mt-10 md:w-full lg:w-4/5  justify-center m-auto ">
+      <div className="flex flex-wrap mt-10 w-full  justify-center m-auto ">
         {portfolio.map(({ img, content, key }) => (
-          <div className="w-96 flex justify-center p-5" key={key}>
+          <div className="max-w-96 w-full sm:w-1/2 lg:w-1/3 flex justify-center p-5" key={key}>
             <WorkCard key={key} img={img} content={content} />
           </div>
         ))}
